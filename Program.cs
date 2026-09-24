cb1b2578379ed3e2c4c486aae1bffef3e2ffd1d3
PAPAours <anael.bleurvacq777@gmail.com>
2026-09-24 09:10:50 +0200

    feat: apply initial Entity Framework migration

--- Program.cs
+++ Program.cs
@@ -3,17 +3,16 @@ using MvcMovie.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configuration du DbContext SQLite
+// Enregistrement du DbContext SQLite dans l'injection de dépendances
 builder.Services.AddDbContext<MvcMovieContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("MvcMovieContext") 
     ?? throw new InvalidOperationException("Connection string 'MvcMovieContext' not found.")));
 
-// Ajout des services MVC
+// Service Controllers + Views
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Pipeline HTTP
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
